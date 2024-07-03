7e5e09dd34c017781b6acd6e06168f03347790b6
Mark Towey <marktowey1@gmail.com>
2024-07-03 11:13:40 +0100

    Created a calculator class in js file including properties and methods

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
 
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+  delete() {}
+  appendNumber(number) {
+    this.currentOperand = number;
+  }
+  appendOperation(operation) {}
+  compute() {}
+  updateDisplay() {
+    this.currentOperandTextElement.innerHTML = this.currentOperand;
+  }
+}
+
+const numberButtons = document.querySelectorAll(".numbers");
+const operationButtons = document.querySelectorAll(".operations");
+const equalsButtons = document.querySelector("#equals");
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
+const previousOperandTextElement = document.querySelector(
+  "#previousOperandText"
+);
+const currentOperandTextElement = document.querySelector("#currentOperandText");
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    currentOperandTextElement.innerHTML = button.innerHTML;
+    //   calculator.appendNumber(button.innerHTML);
+  });
+});
